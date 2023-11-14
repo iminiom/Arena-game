ad26f1850f1407df32ed7a71b03c204be57fe2f3
Elisa N <117632231+iminiom@users.noreply.github.com>
2023-11-14 20:39:35 +0100

    Update hitboxes.py

--- hitboxes.py
+++ hitboxes.py
@@ -10,7 +10,7 @@ class Hitbox:
         self.right = self.xpos + self.width
 
     def go_to(self,x,y):
-        """makes it so that the hitbox moves with the entity"""
+        
         self.xpos = x
         self.ypos = y
         self.bottom = y + self.height
@@ -23,10 +23,7 @@ def is_colliding(hitbox1 : Hitbox,hitbox2 : Hitbox):
     """checks if it is colliding"""
     colliding = False
     
-    if hitbox1.bottom <= hitbox2.bottom and hitbox1.bottom >= hitbox2.top:
-        if hitbox2.right >= hitbox1.left and hitbox2.left <= hitbox1.left:
-            colliding = True 
-    elif hitbox1.right >= hitbox2.left and hitbox1.left <= hitbox2.left:
+    if ((hitbox1.bottom>=hitbox2.top and hitbox1.bottom<=hitbox2.bottom) or (hitbox1.top>=hitbox2.top and hitbox1.top<=hitbox2.bottom)) and ((hitbox1.left>=hitbox2.left and hitbox1.left<=hitbox2.right ) or (hitbox1.right>=hitbox2.left and hitbox1.right<=hitbox2.right)):
         colliding = True
     return colliding
 
@@ -42,6 +39,7 @@ def is_touching(h1 : Hitbox, h2: Hitbox):
         from_where = 'right'
     elif h1.left == h2.right:
         from_where = 'left'
+        
     return from_where 
 
 h1 = Hitbox(0,0,10,10)
